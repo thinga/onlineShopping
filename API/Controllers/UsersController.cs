e7a3e6fce1697795f6769ddf28a691e88afe839f
thinga <32902065+thinga@users.noreply.github.com>
2021-06-30 17:43:13 +0200

    using Dto

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -8,10 +8,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-     [ApiController]
-    [Route("api/[controller]")]
+ 
 
-    public class UsersController :  ControllerBase
+    public class UsersController :  BaseApiController
     {
         private readonly DataContext _context;
 
